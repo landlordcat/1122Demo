4bc275744f9a0439f7b311ff5db7d32083ab5cb0
landlordcat <39575468+landlordcat@users.noreply.github.com>
2018-05-24 11:17:30 +0800

    Create String.cpp
    修改分支2

---
+++ String.cpp
@@ -0,0 +1,21 @@
+String+(const String&s)
+{
+String s1;
+s1=*this;
+s1+=s;
+return s1;
+}
+
+
+String +=(const String&s)
+{
+  char*t=s1;
+  s1=new char[len+s.len+1];
+  strcpy(s1,t);
+  strcat(s1,s.s1);
+  len=(int)strlen[s1];
+  delete [] t;
+  return *this;
+  
+  
+}
